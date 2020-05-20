caf96aa18bc587d6e773877f90dbe5b7b0a4be5e
Loc Thanh <locnt19@gmail.com>
2020-05-20 23:11:54 +0700

    022 turn on CORS

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const routes = require('./routes');
@@ -15,11 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Turn on CORS
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
+app.use(cors());
 
 // routes
 app.use(routes);
